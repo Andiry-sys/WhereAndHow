9e9210fbd738e615518e46feb8db39f9b4e184c7
Andriy Marchyck <marchycandriy@gmail.com>
2026-04-16 17:06:13 +0300

    updated angular to 18v

--- whereandhow.client/src/app/app.module.ts
+++ whereandhow.client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componetes/login/login.component';
 import { RegisterComponent } from './componetes/register/register.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './componetes/home/home.component';
 import { CreateapartamentComponent } from './componetes/createapartament/createapartament.component';
@@ -18,32 +18,26 @@ import {CarouselModule} from "ngx-bootstrap/carousel";
 import { AboutPageComponent } from './componetes/about-page/about-page.component';
 import { AuthInterceptor } from './interceptors/auth-interceptor.interceptor';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    CreateapartamentComponent,
-    UserProfileComponent,
-    ApartmentSearchComponent,
-    NavbarComponent,
-    CurrentApartamentComponent,
-    FooterComponent,
-    AboutPageComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    Ng5SliderModule,
-    CarouselModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        HomeComponent,
+        CreateapartamentComponent,
+        UserProfileComponent,
+        ApartmentSearchComponent,
+        NavbarComponent,
+        CurrentApartamentComponent,
+        FooterComponent,
+        AboutPageComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        Ng5SliderModule,
+        CarouselModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
